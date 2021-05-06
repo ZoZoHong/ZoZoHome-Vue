0e01d0153b262bd6e825086baef80da88303232c
ZoZoHong <1161113466@qq.com>
2021-05-06 16:44:44 +0800

    添加router 添加vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './iconfonts/iconfonts.css'
+import router from './router'
+import store from './store'
 
 
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
